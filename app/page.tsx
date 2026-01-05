8be744938445ae516377fa4d9f313b592010d6dc
arshavardhan17 <2300031000@kluniversity.in>
2026-01-05 21:44:44 +0530

    edited home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 
 export default function Home() {
   return (
-   <div></div>
+   <main className=" dlex min-h-screen flex-col items-center justify-center p-4"> 
+   <div className="w-full max-w-md space-y-8">
+     <div className=" border border-zinc-800 bg-zinc-900 p-6 backdrop-blur-md">
+      <div className="space-y-5">
+        <div className="space-y-2">
+          Create Room
+        </div>
+      </div>
+     </div>
+   </div>
+   </main>
   );
 }
